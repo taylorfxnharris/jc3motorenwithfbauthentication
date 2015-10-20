f24992169b132b149753ad7fe12af333b51f64da
Taylor Harris <thharris10@gmail.com>
2015-10-20 10:22:39 -0400

    saving fb login data

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '1517261768290185',
+		'clientSecret': '236c7483d19aca8d66b36c77da56413c',
+		'callbackURL': '/auth/facebook/callback'
+	},
+}
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'url'	: 'mongodb://thharris10:h00p3r10@ds039504.mongolab.com:39504/jc3motoren'
+}
